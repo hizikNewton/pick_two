7dc842537f4ba8a9b78ed1e55a7938b9e2813868
Bamidele Isaac <bamideleisaac11@gmail.com>
2024-05-06 11:56:53 +0100

    initial commit for pick-two

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+use std::net::TcpListener;
+
+use pick_two::run;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    run(listener)?.await
+}
