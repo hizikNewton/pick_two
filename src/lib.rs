d4c880510c1f8e454a0a87e573b117cd3ddfe69c
Bamidele Isaac <bamideleisaac11@gmail.com>
2024-05-07 00:17:31 +0100

    adding proxy

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,30 @@
 use std::net::TcpListener;
 
 use actix_web::{dev::Server, web, App, HttpResponse, HttpServer};
+use reqwest::Error;
 
 async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
+async fn crawl() -> HttpResponse {
+    log::info!("we are crawling");
+    let response = reqwest::get("https://ng.soccerway.com/")
+        .await
+        .unwrap()
+        .text()
+        .await;
+    log::info!("{:?}", response);
+    HttpResponse::Ok().finish()
+}
+
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .listen(listener)?
-        .run();
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/get_matches", web::get().to(crawl))
+    })
+    .listen(listener)?
+    .run();
     Ok(server)
 }
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use pick_two::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    env_logger::init();
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind random port");
     run(listener)?.await
 }
